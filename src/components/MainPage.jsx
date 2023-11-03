0c75062a9bf8ea4c046e699b4b74f7a28634cc63
SrijanSankrit <sankritsrijan3@gmail.com>
2023-11-04 01:08:44 +0530

    incorrect margin trial #3

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -343,7 +343,7 @@ function LandingInfoAreaComponent() {
                                 fontFamily: 'Nulshock',
                                 fontWeight: 'bold',
                                 textAlign: 'left',
-                                marginLeft: isMobileScreen ? '-14%' : '-15%',
+                                marginLeft: isMobileScreen ? '-13.5%' : '-15%',
                                 marginTop: '-2.5%',
                             }}
                             initial={{ opacity: 0, y: '20vh'}}
@@ -360,7 +360,7 @@ function LandingInfoAreaComponent() {
                                 fontFamily: 'Nulshock',
                                 fontWeight: 'bold',
                                 textAlign: 'left',
-                                marginLeft: isMobileScreen ? '-20.5%' : '-21.5%',
+                                marginLeft: isMobileScreen ? '-20%' : '-21.5%',
                                 marginTop: '-4%',
                             }}
                             initial={{ opacity: 0, y: '20vh'}}
@@ -574,7 +574,7 @@ function LandingInfoAreaComponent() {
                                 fontFamily: 'Nulshock',
                                 fontWeight: 'bold',
                                 textAlign: 'left',
-                                marginLeft: isMobileScreen ? '-21%' : '-22%',
+                                marginLeft: isMobileScreen ? '-20.5%' : '-22%',
                                 marginTop: '-2%'
                             }}
                             initial={{ opacity: 0, y: '20vh'}}
@@ -591,7 +591,7 @@ function LandingInfoAreaComponent() {
                                 fontFamily: 'Nulshock',
                                 fontWeight: 'bold',
                                 textAlign: 'left',
-                                marginLeft: isMobileScreen ? '-22%' : '-23%',
+                                marginLeft: isMobileScreen ? '-21.5%' : '-23%',
                                 marginTop: '-2.5%'
                             }}
                             initial={{ opacity: 0, y: '20vh'}}
