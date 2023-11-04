9d2123722aa0ff0beab2b3690c5c1dceb1417992
Utkarsh Choubey <utkarsh.kc@udaan.com>
2023-11-04 16:36:43 +0530

    minor fix

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { motion } from 'framer-motion';
 import '../index.css';
 
