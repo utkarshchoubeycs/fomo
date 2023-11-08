0dea57f571b58e276b49c297315c8dcd297e284d
SrijanSankrit <sankritsrijan3@gmail.com>
2023-11-08 22:16:31 +0530

    remove showRings state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import RingsAreaComponent from './components/RingsAreaComponent';
 import MainPage from './components/MainPage';
-import { useScroll } from 'framer-motion';
 
 function App() {
 
   const [stage, setStage] = useState(0);
-  const { scrollYProgress } = useScroll();
-  const [showRings, setShowRings] = useState(true);
-
-  useEffect(() => {
-
-    const unSubYProgress = scrollYProgress.on("change", value => {
-
-      // hide RingsComponent when we scroll more than 30%
-      if(value > 0.6) setShowRings(false);
-      else setShowRings(true);
-
-    });
-
-    return () => unSubYProgress();
-  })
 
   return (
     <div className='App'>
-      { showRings === true && <RingsAreaComponent stage={stage} setStage={setStage} /> } 
+      <RingsAreaComponent stage={stage} setStage={setStage} />
       { stage === 4 && <MainPage />}
     </div>
       
