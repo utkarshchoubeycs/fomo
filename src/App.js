9568808f0429bf13808c13c10e4a502146a054e5
SrijanSankrit <sankritsrijan3@gmail.com>
2023-11-08 22:08:52 +0530

    hide rings

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     const unSubYProgress = scrollYProgress.on("change", value => {
 
       // hide RingsComponent when we scroll more than 30%
-      if(value > 0.3) setShowRings(false);
+      if(value > 0.6) setShowRings(false);
       else setShowRings(true);
 
     });
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div className='App'>
-      { showRings === true && <RingsAreaComponent stage={stage} setStage={setStage} /> }
+      { showRings === true && <RingsAreaComponent stage={stage} setStage={setStage} /> } 
       { stage === 4 && <MainPage />}
     </div>
       
